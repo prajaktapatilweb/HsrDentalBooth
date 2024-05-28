e07ac383c9a441c402f08d12bae6297542f138c5
prajaktpatilaweb <prajaktaweb@gmail.com>
2024-05-28 14:21:10 +0530

    remove unnessaery data

--- components/home/LabTabs.js
+++ components/home/LabTabs.js
@@ -4,10 +4,8 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import Countdown from './Countdown';
 import Clinicinfo from './Clinicinfo';
 import { Container } from '@mui/material';
-import Clinicinfo1 from './Clinicinfo1';
 
 export default function LabTabs() {
   const [value, setValue] = React.useState('1');
@@ -27,11 +25,9 @@ export default function LabTabs() {
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
             <Tab label="Thane" value="1" />
-            <Tab label="Airoli" value="2" />
           </TabList>
         </Box>
         <TabPanel value="1"> <Clinicinfo/>  </TabPanel>
-        <TabPanel value="2"><Clinicinfo1/> </TabPanel>
       </TabContext>
     </Box>
 </>
